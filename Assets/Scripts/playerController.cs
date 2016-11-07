94c8cd0b1123fca1ff1a1fba20627b18d3743d79
Juan M Rodriguez Romero <rjuan08@gmail.com>
2016-11-07 11:28:10 -0800

    First commit.

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+    //movement variables
+    public float maxSpeed;
+
+    Rigidbody2D myRB;
+    Animator myAnim; //change the parameters of the animator
+    bool facingRight;
+
+	// Use this for initialization
+	void Start () {
+        myRB = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+        facingRight = true; //initialize character facing right to true
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float move = Input.GetAxis("Horizontal");
+        myAnim.SetFloat("speed", Mathf.Abs(move));
+        myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y);
+
+        if(move > 0 && !facingRight) //change the facing direction of the character
+        {
+            flip();
+        }
+        else if(move < 0 && facingRight)
+        {
+            flip();
+        }
+
+	}
+
+    void flip()
+    {
+        facingRight = !facingRight; //do the oposite
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
